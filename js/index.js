966598797511d8bbfafde55c00f1c3baf6e5e769
Leffis <leffisc@gmail.com>
2020-01-05 03:48:16 -0500

    debug issue

--- js/index.js
+++ js/index.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
 
     $('#submit').click(function() {
 
-        event.preventDefault(); // prevent PageReLoad
+        //event.preventDefault(); // prevent PageReLoad
 
         var ValidEmail = $('#username').val() === 'Linna'; // User validate
         var ValidPassword = $('#password').val() === '55'; // Password validate
 
         if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
-            window.location = 'mainpage.html'; // go to home.html
+            window.location = '../mainpage.html'; // go to home.html
         }
         else {
             $('.error').css('display', 'block'); // show error msg
