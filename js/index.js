5a3b9198376771ba06b664df1a3388e7d250ea64
Leffis <leffisc@gmail.com>
2020-01-05 04:53:40 -0500

    Update index.html

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
         if (ValidEmail === true && ValidPassword === true) { // if ValidEmail & ValidPassword
             $('.valid').css('display', 'block');
-            window.location = '../mainpage.html'; // go to home.html
+            window.location = 'mainpage.html'; // go to home.html
         }
         else {
             $('.error').css('display', 'block'); // show error msg
